efb3d6d3148ab189ce021ce165228a1d5eefee75
Praveen <pattarpraveen15@gmail.com>
2024-05-15 00:06:34 +0530

    room db files setup, ui done for main activity

---
+++ app/src/main/java/com/myapps/notebase/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.myapps.notebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity() {
+    lateinit var notesRV: RecyclerView
+    lateinit var addFAB: FloatingActionButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        notesRV = findViewById(R.id.idRVNotes)
+        addFAB = findViewById(R.id.idFABAddNotes)
+
+    }
+}
